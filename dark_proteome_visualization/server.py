66ebc03175a7b047e96a54e2b17929fdef559248
Zethson <lukas.heumos@gmail.com>
2018-12-17 23:22:35 +0100

    [FEATURE] Replace argparse with click

--- dark_proteome_visualization/server.py
+++ dark_proteome_visualization/server.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from argparse import ArgumentParser
+import click
 from contextlib import suppress
 
 from sqlalchemy import create_engine
@@ -12,38 +12,36 @@ from gevent.pywsgi import WSGIServer
 from .app import app
 
 
-log = logging.getLogger()
+console = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console.setFormatter(formatter)
+LOG = logging.getLogger("Server")
+LOG.addHandler(console)
+LOG.setLevel(logging.INFO)
 
 CURRENT_DIR = os.path.abspath(os.getcwd())
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_PATH = os.path.join(MODULE_DIR, 'static')
 TEMPLATES_PATH = os.path.join(MODULE_DIR, 'templates')
 
-parser = ArgumentParser()
-parser.add_argument("-d", "--debug", action='store_true', help="Enable Flask debugger")
-parser.add_argument("-L", "--listen", default='127.0.0.1', help="Listen this address for HTTP")
-parser.add_argument("-P", "--port", default=8000, help="Listen this port for HTTP", type=int)
-parser.add_argument('--log-level', help='Set logging level', default='info',
-                    choices=('critical', 'fatal', 'error', 'warning', 'warn', 'info', 'debug'))
-
-def main():
-    options = parser.parse_args()
-
-    logging.basicConfig(
-        level=getattr(logging, options.log_level.upper(), logging.INFO),
-        format="[%(asctime)s] %(filename)s:%(lineno)d %(levelname)s %(message)s"
-    )
-
+@click.command()
+@click.option('-d', '--debug',
+              help='Enable Flask debugger', required=False)
+@click.option('-l', '--listen', default='127.0.0.1',
+              help='Listen to this address for HTTP', required=False)
+@click.option('-p', '--port', default=8000,
+              help='Listen this port for HTTP', required=False)
+def main(debug, listen, port):
     app.template_folder = TEMPLATES_PATH
     app.static_folder = STATIC_PATH
 
-    log.info("Starting service on http://%s:%d/", options.listen, options.port)
+    LOG.info("Starting service on http://%s:%d/", listen, port)
 
-    if options.debug:
-        log.warning("Running on debug mode not for production.")
-        app.run(host=options.listen, port=options.port, debug=True)
+    if debug:
+        LOG.warning("Running on debug mode not for production.")
+        app.run(host=listen, port=port, debug=True)
     else:
-        http_server = WSGIServer((options.listen, options.port), app)
+        http_server = WSGIServer((listen, port), app)
 
         with suppress(KeyboardInterrupt):
             http_server.serve_forever()
