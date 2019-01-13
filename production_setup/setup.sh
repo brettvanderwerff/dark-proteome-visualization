283717235f34f00cdbc68b23a6bb85b480b5e92e
brett <vande060@gmail.com>
2019-01-12 19:00:26 -0800

    add production files folder

---
+++ production_setup/setup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Reference:
+# https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-ubuntu-18-04
+
+# add new user brett as sudouser
+adduser brett
+
+usermod -aG sudo brett
+
+su brett
+
+# update the apt package manager
+
+apt-get update
+
+# install pip, python3, and nginx web server
+
+apt-get install python3-pip python3-dev nginx
+
+pip3 install virtualenv
+
+cd ~
+
+git clone https://github.com/brettvanderwerff/dark-proteome-visualization
+
+cd dark-proteome-visualization
+
+virtualenv dpenv
+
+source dpenv/bin/activate
+
+pip install gunicorn
+
+python setup.py clean --all install
+
+cp /home/brett/dark-proteome-visualization/production_setup/dark_proteome_visualization.service \
+/etc/systemd/system/dark_proteome_visualization.service
+
+systemctl start dark_proteome_visualization
+
+systemctl enable dark_proteome_visualization
+
+cp /home/brett/dark-proteome-visualization/production_setup/dark_proteome_visualization \
+/etc/nginx/sites-available/dark_proteome_visualization
+
+ln -s /etc/nginx/sites-available/dark_proteome_visualization /etc/nginx/sites-enabled
+
+nginx -t
+
+systemctl restart nginx
+
+ufw delete allow 5000
+
+ufw allow 'Nginx Full'
+
+
+
+
+
+
+
